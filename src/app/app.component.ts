48e8bc3557e9e3b467fce8d01c4ad72a612e55a6
nickariley <nickariley@gmail.com>
2020-08-12 19:44:47 -0500

    add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'Todo List';
   todoList: any[] = [];
+  todoTitle = "";
 
   ngOnInit(): void{
     this.todoList = [
